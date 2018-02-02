59d9fc7488f7f5ed70b0a90532e564f0890896b2
AyadiAkrem <32717598+AyadiAkrem@users.noreply.github.com>
2018-02-02 10:14:58 +0100

    Add an example related to the overload of methods. This example shows how the compiler interpret the overloaded methods where there parameters are mixed between primitive variable (int), its wrapper class (Integer) and the Object.

---
+++ OCA8/src/com/oca8/training/chapter3/methodsparameter/MethodsOverload_2.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oca8.training.chapter3.methodsparameter;
+
+/**
+ * This class cover the following point:
+ * </br> [6.1] Create methods with arguments and return values; including
+ * overloaded methods
+ *
+ *
+ * @author AYADI Akrem
+ */
+public class MethodsOverload_2 {
+
+    /**
+     * Tacking an Integer as parameter
+     *
+     * @param parameter
+     * @return
+     */
+    public int myOverloadedMethod(Integer parameter) {
+        return 0;
+    }
+
+    public int myOverloadedMethod(Object parameter) {
+        return 1;
+    }
+
+    public int myOverloadedMethod(Integer... parameters) {
+        return 2;
+    }
+
+    public int myOverloadedMethod(Object parameter, Integer... parameters) {
+        return 3;
+    }
+
+    public int myOverloadedMethod(Integer parameter, Object... parameters) {
+        return 4;
+    }
+
+    public int myOverloadedMethod(int... parameters) {
+        return 5;
+    }
+
+    public int myOverloadedMethod(Object parameter, int... parameters) {
+        return 6;
+    }
+
+    public int myOverloadedMethod(int parameter) {
+        return 7;
+    }
+
+    public int myOverloadedMethod() {
+        return 8;
+    }
+
+}
---
+++ OCA8/test/com/oca8/training/chapter3/methodsparameter/MethodsOverloadTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oca8.training.chapter3.methodsparameter;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author EXG503
+ */
+public class MethodsOverloadTest {
+
+    public MethodsOverloadTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of myOverloadedMethod method, of class MethodsOverload.
+     */
+    @Test
+    public void testMyOverloadedMethod() {
+        System.out.println("myOverloadedMethod");
+        Integer parameter = 2;
+        MethodsOverload instance = new MethodsOverload();
+        assertEquals(0, instance.myOverloadedMethod(parameter));
+        assertEquals(1, instance.myOverloadedMethod((Object) parameter));
+        assertEquals(2, instance.myOverloadedMethod(parameter, parameter));
+        assertEquals(3, instance.myOverloadedMethod((Object) parameter, parameter, parameter));
+        assertEquals(3, instance.myOverloadedMethod(new Object(), parameter, parameter));
+        assertEquals(6, instance.myOverloadedMethod(new Object(), 2, 3));
+        assertEquals(3, instance.myOverloadedMethod(new Object(), new Integer[]{}));
+        assertEquals(4, instance.myOverloadedMethod(parameter, (Object) parameter));
+        assertEquals(5, instance.myOverloadedMethod(new int[]{1, 3}));
+        assertEquals(2, instance.myOverloadedMethod(new Integer[]{1, 3}));
+        assertEquals(5, instance.myOverloadedMethod(new int[]{}));
+        assertEquals(2, instance.myOverloadedMethod(new Integer[]{}));
+        assertEquals(5, instance.myOverloadedMethod(1, 3));
+        assertEquals(7, instance.myOverloadedMethod(1));
+        assertEquals(7, instance.myOverloadedMethod());
+    }
+
+   
+}
